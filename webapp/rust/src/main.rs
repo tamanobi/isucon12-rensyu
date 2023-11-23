a417dbcc4eb1f10c6a1e0b71eea8b4fe2643ceba
do7be <bomsap700@gmail.com>
2023-11-23 19:52:18 +0900

    もどした

--- webapp/rust/src/main.rs
+++ webapp/rust/src/main.rs
@@ -712,8 +712,9 @@ async fn billing_report_by_competition(
         billing_map.insert(vh.player_id, "visitor");
     }
 
+    // 多分トランザクションはるとかしたほうがいい
     // player_scoreを読んでいる時に更新が走ると不整合が起こるのでロックを取得する
-    // let _fl = flock_by_tenant_id(tenant_id).await?;
+    let _fl = flock_by_tenant_id(tenant_id).await?;
 
     // スコアを登録した参加者のIDを取得する
     sqlx::query_as(
